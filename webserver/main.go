a8386cf4b94bd752451f54127a07788991a2836a
ice <tony_mistark@163.com>
2018-12-16 00:19:19 +0800

    change text

--- webserver/main.go
+++ webserver/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "<h1>Hello, i am God!</h1>")
+    io.WriteString(w, "<h1>Hello World, i am God!</h1>")
 }
 
 func main() {
